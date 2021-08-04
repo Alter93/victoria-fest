2147f368b284e476b003192bb804698df9572e72
Alter93 <alexae93@gmail.com>
2021-08-04 13:47:45 -0500

    / y /registro apuntan a prerregistro

--- conferencias/urls.py
+++ conferencias/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from conferencias import views
 
 urlpatterns = [
+    path('', views.prerregistro, name='prerregistro'),
     path('registro', views.prerregistro, name='prerregistro'),
 ]
