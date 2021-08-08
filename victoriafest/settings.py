4daf6dabfe5920fc57892dfd2986a1120807ff1d
Alter93 <alexae93@gmail.com>
2021-08-07 20:45:35 -0500

    primer version usuarios

--- victoriafest/settings.py
+++ victoriafest/settings.py
@@ -131,3 +131,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alexae93@gmail.com'
+EMAIL_HOST_PASSWORD = 'ufzbfsiufzrkbgsc'
