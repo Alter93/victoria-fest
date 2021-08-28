c01a0324bce9fd952d91e23d2d3391093392ac14
Alter93 <alexae93@gmail.com>
2021-08-28 12:33:56 -0500

    mover home a paginahome

--- conferencias/urls.py
+++ conferencias/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from conferencias import views
 
 urlpatterns = [
-    #path('', views.prerregistro, name='prerregistro'),
+    path('', views.prerregistro, name='prerregistro'),
     path('registro', views.prerregistro, name='prerregistro'),
-    path('', views.home, name='home'),
+    path('paginahome', views.home, name='home'),
 ]
